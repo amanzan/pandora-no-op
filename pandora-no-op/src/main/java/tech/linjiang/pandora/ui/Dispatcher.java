8208704d407a60ac3058dbf488ab25f9e46d7051
Alberto Manzano Torregrosa <alberto@codelesslabs.com>
2021-07-20 17:05:31 +0200

    added ui, dispatcher, start and type to no-op

---
+++ pandora-no-op/src/main/java/tech/linjiang/pandora/ui/Dispatcher.java
@@ -0,0 +1,12 @@
+package tech.linjiang.pandora.ui;
+
+import android.content.Context;
+
+import tech.linjiang.pandora.ui.connector.Type;
+
+public class Dispatcher {
+
+    public void start(Context context, Type type) {
+
+    }
+}
